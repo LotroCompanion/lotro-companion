ebdf786b03369bec6f511c8295957548aa2d05e3
Damien Morcellet <dmorcellet@free.fr>
2013-01-05 16:23:41 +0000

    delta4j/lotro:     * font manager and custom font attempt

---
+++ src/main/java/delta/games/lotro/gui/utils/FontsManager.java
@@ -0,0 +1,63 @@
+package delta.games.lotro.gui.utils;
+
+import java.awt.Font;
+import java.awt.GraphicsEnvironment;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+/**
+ * Custom fonts manager.
+ * @author DAM
+ */
+public class FontsManager
+{
+  private final String font = "/resources/gui/fonts/firstv2.ttf";
+
+  private static FontsManager instance=new FontsManager();
+
+  private Font _font;
+
+  /**
+   * Get the sole instance of this class.
+   * @return the fonts manager.
+   */
+  public static FontsManager getInstance()
+  {
+    return instance;
+  }
+
+  private FontsManager()
+  {
+    loadFont();
+  }
+
+  private void loadFont()
+  {
+    try {
+      URL ufont=FontsManager.class.getResource(font);
+      URLConnection con = ufont.openConnection();
+      con.connect();
+      InputStream is = con.getInputStream();
+      Font f = Font.createFont(Font.TRUETYPE_FONT, is);
+      GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+      ge.registerFont(f);
+      String s = f.getFamily();
+      System.out.println(s);
+      _font = new Font(s, Font.PLAIN, 12);
+    }
+    catch(Exception e)
+    {
+      e.printStackTrace();
+    }
+  }
+  
+  /**
+   * Get the custom font.
+   * @return a font.
+   */
+  public Font getFont()
+  {
+    return _font;
+  }
+}
