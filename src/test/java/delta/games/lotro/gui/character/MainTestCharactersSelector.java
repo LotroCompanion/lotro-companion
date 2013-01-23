f54c711c3a0e1d71247ea2c38f1ef4e40afe2554
Damien Morcellet <dmorcellet@free.fr>
2013-01-23 19:32:33 +0000

    delta4j/lotro:     * (added missing entries from the previous commit...)     * characters selection GUI (test code)     * moved warbands definitions to the 'lore' directory instead of 'config'

---
+++ src/test/java/delta/games/lotro/gui/character/MainTestCharactersSelector.java
@@ -0,0 +1,29 @@
+package delta.games.lotro.gui.character;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import delta.games.lotro.character.CharacterFile;
+import delta.games.lotro.character.log.LotroTestUtils;
+
+/**
+ * @author DAM
+ */
+public class MainTestCharactersSelector
+{
+  /**
+   * Basic main method for test.
+   * @param args Not used.
+   */
+  public static void main(String[] args)
+  {
+    LotroTestUtils utils=new LotroTestUtils();
+    List<CharacterFile> toons=utils.getAllFiles();
+    List<CharacterFile> selectedToons=new ArrayList<CharacterFile>();
+    selectedToons=CharactersSelectorWindowController.selectToons(null,toons,selectedToons);
+    for(CharacterFile toon : selectedToons)
+    {
+      System.out.println(toon.getIdentifier());
+    }
+  }
+}
--- src/test/java/delta/games/lotro/gui/stats/warbands/MainTestShowWarbandsStatistics.java
+++ src/test/java/delta/games/lotro/gui/stats/warbands/MainTestShowWarbandsStatistics.java
@@ -1,7 +1,5 @@
 package delta.games.lotro.gui.stats.warbands;
 
-import delta.games.lotro.character.CharacterFile;
-import delta.games.lotro.character.log.LotroTestUtils;
 import delta.games.lotro.stats.warbands.MultipleToonsWarbandsStats;
 
 /**
@@ -17,15 +15,6 @@ public class MainTestShowWarbandsStatistics
   public static void main(String[] args)
   {
     MultipleToonsWarbandsStats stats=new MultipleToonsWarbandsStats();
-    LotroTestUtils utils=new LotroTestUtils();
-    CharacterFile allyriel=utils.getToonByName("Allyriel");
-    stats.addToon(allyriel);
-    CharacterFile allurwyn=utils.getToonByName("Allurwyn");
-    stats.addToon(allurwyn);
-    CharacterFile serilis=utils.getToonByName("Serilis");
-    stats.addToon(serilis);
-    CharacterFile glumlug=utils.getToonByName("Glumlug");
-    stats.addToon(glumlug);
     WarbandsWindowController controller=new WarbandsWindowController(stats);
     controller.show();
   }
