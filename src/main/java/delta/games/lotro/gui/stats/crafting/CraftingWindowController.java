eeedeaad9066cce5c28914fae0bd00ecd6fb0432
Damien Morcellet <dmorcellet@free.fr>
2017-09-15 19:08:35 +0200

    Added a combo to update the current vocation

--- src/main/java/delta/games/lotro/gui/stats/crafting/CraftingWindowController.java
+++ src/main/java/delta/games/lotro/gui/stats/crafting/CraftingWindowController.java
@@ -1,10 +1,13 @@
 package delta.games.lotro.gui.stats.crafting;
 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.util.HashMap;
+import java.util.List;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
@@ -13,12 +16,15 @@ import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 import delta.common.ui.swing.GuiFactory;
+import delta.common.ui.swing.combobox.ComboBoxController;
+import delta.common.ui.swing.combobox.ItemSelectionListener;
 import delta.common.ui.swing.windows.DefaultWindowController;
 import delta.games.lotro.character.CharacterFile;
 import delta.games.lotro.character.crafting.CraftingStatus;
 import delta.games.lotro.character.crafting.ProfessionStatus;
 import delta.games.lotro.crafting.Profession;
 import delta.games.lotro.crafting.Vocation;
+import delta.games.lotro.crafting.Vocations;
 
 /**
  * Controller for a "crafting stats" window.
@@ -28,6 +34,8 @@ public class CraftingWindowController extends DefaultWindowController
 {
   private CharacterFile _toon;
   private CraftingStatus _stats;
+  private ComboBoxController<Vocation> _vocation;
+  private JPanel _professionsPanel;
   private HashMap<Profession,ProfessionPanelController> _panels;
 
   /**
@@ -58,19 +66,54 @@ public class CraftingWindowController extends DefaultWindowController
   protected JComponent buildContents()
   {
     JPanel panel=GuiFactory.buildPanel(new GridBagLayout());
-    // Vocation label
+    // Vocation panel
+    // - combo
+    _vocation=buildVocationCombo();
     Vocation vocation=_stats.getVocation();
-    String vocationStr="Vocation: "+((vocation!=null)?vocation.getName():"-");
-    JLabel vocationLabel=GuiFactory.buildLabel(vocationStr);
+    JPanel vocationPanel=GuiFactory.buildPanel(new FlowLayout());
+    JLabel vocationLabel=GuiFactory.buildLabel("Vocation:");
+    vocationPanel.add(vocationLabel);
+    vocationPanel.add(_vocation.getComboBox());
 
+    // Professions panel
+    _professionsPanel=GuiFactory.buildPanel(new BorderLayout());
+    GridBagConstraints c=new GridBagConstraints(0,0,1,1,0,0,GridBagConstraints.WEST,GridBagConstraints.NONE,new Insets(5,10,0,10),0,0);
+    panel.add(vocationPanel,c);
+    GridBagConstraints c2=new GridBagConstraints(0,1,1,1,1.0,1.0,GridBagConstraints.WEST,GridBagConstraints.BOTH,new Insets(5,10,5,10),0,0);
+    panel.add(_professionsPanel,c2);
+
+    ItemSelectionListener<Vocation> vocationListener=new ItemSelectionListener<Vocation>()
+    {
+      public void itemSelected(Vocation selectedVocation)
+      {
+        initProfessionPanel(selectedVocation);
+      }
+    };
+    _vocation.addListener(vocationListener);
+    _vocation.selectItem(vocation);
+
+    return panel;
+  }
+
+  private void initProfessionPanel(Vocation selectedVocation)
+  {
     JComponent centerComponent=null;
-    Profession[] professions=_stats.getProfessions();
+    boolean wasEmpty=(_professionsPanel.getComponentCount()==0);
+    _professionsPanel.removeAll();
+    Vocation currentVocation=_stats.getVocation();
+    if (currentVocation!=selectedVocation)
+    {
+      long now=System.currentTimeMillis();
+      _stats.resetVocation(selectedVocation,now);
+      disposeProfessionPanels();
+    }
+    Profession[] professions=(selectedVocation!=null)?selectedVocation.getProfessions():null;
     if ((professions!=null) && (professions.length>0))
     {
       JTabbedPane tabbedPane=GuiFactory.buildTabbedPane();
       for(Profession profession : professions)
       {
-        ProfessionStatus stats=_stats.getProfessionStatus(profession);
+        ProfessionStatus stats=_stats.getProfessionStatus(profession,true);
         ProfessionPanelController craftingPanelController=new ProfessionPanelController(stats);
         JPanel craftingPanel=craftingPanelController.getPanel();
         tabbedPane.add(profession.getLabel(),craftingPanel);
@@ -83,11 +126,13 @@ public class CraftingWindowController extends DefaultWindowController
       JLabel centerLabel=new JLabel("No vocation!");
       centerComponent=centerLabel;
     }
-    GridBagConstraints c=new GridBagConstraints(0,0,1,1,0,0,GridBagConstraints.WEST,GridBagConstraints.NONE,new Insets(5,10,0,10),0,0);
-    panel.add(vocationLabel,c);
-    GridBagConstraints c2=new GridBagConstraints(0,1,1,1,1.0,1.0,GridBagConstraints.WEST,GridBagConstraints.BOTH,new Insets(5,10,5,10),0,0);
-    panel.add(centerComponent,c2);
-    return panel;
+    _professionsPanel.add(centerComponent,BorderLayout.CENTER);
+    _professionsPanel.revalidate();
+    _professionsPanel.repaint();
+    if (!wasEmpty)
+    {
+      getFrame().pack();
+    }
   }
 
   @Override
@@ -113,6 +158,27 @@ public class CraftingWindowController extends DefaultWindowController
     return id;
   }
 
+  private ComboBoxController<Vocation> buildVocationCombo()
+  {
+    ComboBoxController<Vocation> ret=new ComboBoxController<Vocation>();
+    ret.addEmptyItem("");
+    List<Vocation> vocations=Vocations.getInstance().getAll();
+    for(Vocation vocation : vocations)
+    {
+      ret.addItem(vocation,vocation.getName());
+    }
+    return ret;
+  }
+
+  private void disposeProfessionPanels()
+  {
+    for(ProfessionPanelController controller : _panels.values())
+    {
+      controller.dispose();
+    }
+    _panels.clear();
+  }
+
   /**
    * Release all managed resources.
    */
@@ -122,11 +188,7 @@ public class CraftingWindowController extends DefaultWindowController
     super.dispose();
     if (_panels!=null)
     {
-      for(ProfessionPanelController controller : _panels.values())
-      {
-        controller.dispose();
-      }
-      _panels.clear();
+      disposeProfessionPanels();
       _panels=null;
     }
     _stats=null;
