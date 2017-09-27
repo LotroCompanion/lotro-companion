54c301990c9d10485f249be56ce3f78da1277931
Damien Morcellet <dmorcellet@free.fr>
2017-09-27 17:51:08 +0200

    Added a chart controller to display stats contributions

---
+++ src/main/java/delta/games/lotro/gui/character/stats/contribs/StatContribsChartPanelController.java
@@ -0,0 +1,64 @@
+package delta.games.lotro.gui.character.stats.contribs;
+
+import java.text.DecimalFormat;
+import java.util.List;
+
+import javax.swing.JPanel;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
+import org.jfree.chart.plot.PiePlot;
+import org.jfree.data.general.DefaultPieDataset;
+
+import delta.games.lotro.character.stats.STAT;
+import delta.games.lotro.character.stats.contribs.SourceContribution;
+import delta.games.lotro.utils.FixedDecimalsInteger;
+
+/**
+ * Controller for a chart that show stat contributions.
+ * @author DAM
+ */
+public class StatContribsChartPanelController
+{
+  private JPanel _panel;
+  private DefaultPieDataset _data;
+
+  /**
+   * Constructor.
+   * @param stat Targeted stat.
+   */
+  public StatContribsChartPanelController(STAT stat)
+  {
+    _data=new DefaultPieDataset();
+    JFreeChart pieChart = ChartFactory.createPieChart(stat.getName(), _data, true, true, true);
+    PiePlot plot=(PiePlot)pieChart.getPlot();
+    plot.setLabelGenerator(new StandardPieSectionLabelGenerator("{0}={1} ({2})",new DecimalFormat("#0.#"),new DecimalFormat("#0.##%")));
+    _panel = new ChartPanel(pieChart);
+  }
+
+  /**
+   * Get the managed panel.
+   * @return a panel.
+   */
+  public JPanel getPanel()
+  {
+    return _panel;
+  }
+
+  /**
+   * Set the contributions to display.
+   * @param contribs Contributions to display.
+   */
+  public void setContributions(List<SourceContribution> contribs)
+  {
+    _data.clear();
+    for(SourceContribution contrib : contribs)
+    {
+      String source=contrib.getSource();
+      FixedDecimalsInteger value=contrib.getValue();
+      _data.setValue(source,value.doubleValue());
+    }
+  }
+}
---
+++ src/test/java/delta/games/lotro/gui/character/stats/contribs/MainTestStatContribsChart.java
@@ -0,0 +1,66 @@
+package delta.games.lotro.gui.character.stats.contribs;
+
+import java.awt.BorderLayout;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+import delta.common.ui.swing.GuiFactory;
+import delta.common.ui.swing.windows.DefaultWindowController;
+import delta.games.lotro.character.CharacterData;
+import delta.games.lotro.character.stats.BasicStatsSet;
+import delta.games.lotro.character.stats.CharacterGenerationTools;
+import delta.games.lotro.character.stats.CharacterGeneratorGiswald;
+import delta.games.lotro.character.stats.CharacterStatsComputer;
+import delta.games.lotro.character.stats.STAT;
+import delta.games.lotro.character.stats.contribs.SourceContribution;
+import delta.games.lotro.character.stats.contribs.StatsContributionsManager;
+
+/**
+ * Test for the stat contributions chart.
+ * @author DAM
+ */
+public class MainTestStatContribsChart
+{
+  private void doIt()
+  {
+    CharacterGenerationTools tools=new CharacterGenerationTools();
+    CharacterGeneratorGiswald generator=new CharacterGeneratorGiswald(tools);
+    CharacterData c=generator.buildCharacter();
+    doIt(c);
+  }
+
+  private void doIt(CharacterData data)
+  {
+    StatsContributionsManager contribs=new StatsContributionsManager();
+    CharacterStatsComputer statsComputer=new CharacterStatsComputer(contribs);
+    BasicStatsSet stats=statsComputer.getStats(data);
+    System.out.println(stats);
+    Map<STAT,List<SourceContribution>> sortedContribs=contribs.sortByStat();
+    JPanel panel=GuiFactory.buildBackgroundPanel(new BorderLayout());
+    JTabbedPane tabs=GuiFactory.buildTabbedPane();
+    panel.add(tabs,BorderLayout.CENTER);
+    for(STAT stat : sortedContribs.keySet())
+    {
+      StatContribsChartPanelController chartController=new StatContribsChartPanelController(stat);
+      chartController.setContributions(sortedContribs.get(stat));
+      JPanel statPanel=chartController.getPanel();
+      tabs.add(stat.getName(),statPanel);
+    }
+    DefaultWindowController w=new DefaultWindowController();
+    w.getFrame().add(panel);
+    w.getFrame().pack();
+    w.show();
+  }
+
+  /**
+   * Main method for this test.
+   * @param args Not used.
+   */
+  public static void main(String[] args)
+  {
+    new MainTestStatContribsChart().doIt();
+  }
+}
