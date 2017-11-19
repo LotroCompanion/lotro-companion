24f0b8de17c0bbe3eeb17e1591148c826a764e9b
Damien Morcellet <dmorcellet@free.fr>
2017-11-19 22:24:05 +0100

    Fixed the background of the title of the stat contribs chart

--- src/main/java/delta/games/lotro/gui/character/stats/contribs/StatContribsChartPanelController.java
+++ src/main/java/delta/games/lotro/gui/character/stats/contribs/StatContribsChartPanelController.java
@@ -1,5 +1,7 @@
 package delta.games.lotro.gui.character.stats.contribs;
 
+import java.awt.Color;
+import java.awt.Paint;
 import java.text.DecimalFormat;
 
 import javax.swing.JPanel;
@@ -9,8 +11,10 @@ import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
 import org.jfree.chart.plot.PiePlot;
+import org.jfree.chart.title.TextTitle;
 import org.jfree.data.general.DefaultPieDataset;
 
+import delta.common.ui.swing.GuiFactory;
 import delta.games.lotro.character.stats.STAT;
 import delta.games.lotro.character.stats.contribs.ContribsByStat;
 import delta.games.lotro.character.stats.contribs.StatContribution;
@@ -37,9 +41,12 @@ public class StatContribsChartPanelController
     boolean legend=false;
     boolean urls=false;
     _pieChart = ChartFactory.createPieChart("Stat name", _data, legend, true, urls);
+    Paint backgroundPaint=GuiFactory.getBackgroundPaint();
+    _pieChart.setBackgroundPaint(backgroundPaint);
     PiePlot plot=(PiePlot)_pieChart.getPlot();
     plot.setLabelGenerator(new StandardPieSectionLabelGenerator("{0}={1} ({2})",new DecimalFormat("#0.#"),new DecimalFormat("#0.##%")));
     _panel = new ChartPanel(_pieChart);
+    _panel.setOpaque(false);
   }
 
   /**
@@ -63,7 +70,7 @@ public class StatContribsChartPanelController
     STAT stat=contribs.getStat();
     if (stat!=null)
     {
-      _pieChart.setTitle(stat.getName());
+      setTitle(stat.getName());
     }
     for(StatContribution contrib : contribs.getContribs())
     {
@@ -71,6 +78,15 @@ public class StatContribsChartPanelController
       FixedDecimalsInteger value=contrib.getValue();
       _data.setValue(source,value.doubleValue());
     }
+  }
+
+  private void setTitle(String title)
+  {
+    TextTitle t=new TextTitle(title);
+    t.setFont(t.getFont().deriveFont(24.0f));
+    Color foregroundColor=GuiFactory.getForegroundColor();
+    t.setPaint(foregroundColor);
+    _pieChart.setTitle(t);
   }
 
   /**
