76e0a93207b2ad72cf9eae4a7d729f2843efd4c6
Damien Morcellet <dmorcellet@free.fr>
2018-08-29 17:27:29 +0200

    Added a 'date' format (in addition to date/time format)

--- src/main/java/delta/games/lotro/utils/DateFormat.java
+++ src/main/java/delta/games/lotro/utils/DateFormat.java
@@ -10,7 +10,7 @@ import delta.common.ui.swing.text.dates.DateCodec;
  */
 public class DateFormat
 {
-  private static DateCodec _codec=new DateCodec()
+  private static DateCodec _dateTimeCodec=new DateCodec()
   {
     @Override
     public Long parseDate(String dateStr)
@@ -21,7 +21,7 @@ public class DateFormat
     @Override
     public String formatDate(Long date)
     {
-      return DateFormat.formatDate(date);
+      return DateFormat.formatDateTime(date);
     }
   };
 
@@ -31,10 +31,10 @@ public class DateFormat
    */
   public static DateCodec getDateTimeCodec()
   {
-    return _codec;
+    return _dateTimeCodec;
   }
 
-  private static String formatDate(Long date)
+  private static String formatDateTime(Long date)
   {
     if (date!=null)
     {
@@ -53,6 +53,57 @@ public class DateFormat
     {
       int length=dateStr.length();
       if (length!=16) // DD/MM/YYYY HH:MM
+      {
+        return null;
+      }
+    }
+    Date date=Formats.parseDate(dateStr);
+    return (date!=null)?Long.valueOf(date.getTime()):null;
+  }
+
+  private static DateCodec _dateCodec=new DateCodec()
+  {
+    @Override
+    public Long parseDate(String dateStr)
+    {
+      return parseDateString(dateStr,true);
+    }
+
+    @Override
+    public String formatDate(Long date)
+    {
+      return DateFormat.formatDate(date);
+    }
+  };
+
+  /**
+   * Get the date codec.
+   * @return the date codec.
+   */
+  public static DateCodec getDateCodec()
+  {
+    return _dateCodec;
+  }
+
+  private static String formatDate(Long date)
+  {
+    if (date!=null)
+    {
+      return Formats.getDateString(new Date(date.longValue()));
+    }
+    return "";
+  }
+
+  private static Long parseDateString(String dateStr, boolean strict)
+  {
+    if (dateStr==null)
+    {
+      return null;
+    }
+    if (strict)
+    {
+      int length=dateStr.length();
+      if (length!=10) // DD/MM/YYYY
       {
         return null;
       }
