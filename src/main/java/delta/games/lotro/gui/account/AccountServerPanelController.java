f0970b46de3ab0b1d046ad06bec72a89301a5397
Damien Morcellet <dmorcellet@free.fr>
2018-08-29 09:57:10 +0200

    Account window: use one tab by server

---
+++ src/main/java/delta/games/lotro/gui/account/AccountServerPanelController.java
@@ -0,0 +1,169 @@
+package delta.games.lotro.gui.account;
+
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+
+import delta.common.ui.swing.GuiFactory;
+import delta.common.ui.swing.windows.WindowController;
+import delta.common.ui.swing.windows.WindowsManager;
+import delta.games.lotro.account.Account;
+import delta.games.lotro.account.AccountUtils;
+import delta.games.lotro.character.CharacterFile;
+import delta.games.lotro.gui.character.storage.AccountStorageDisplayWindowController;
+import delta.games.lotro.gui.toon.ToonsTableController;
+
+/**
+ * Controller for a panel to display account/server data.
+ * @author DAM
+ */
+public class AccountServerPanelController implements ActionListener
+{
+  private static final String STORAGE_COMMAND="storage";
+
+  // Data
+  private Account _account;
+  private String _server;
+  // UI
+  private JPanel _panel;
+  private WindowsManager _windowsManager;
+  // Controllers
+  private WindowController _parent;
+  private ToonsTableController _toonsTable;
+
+  /**
+   * Constructor.
+   * @param parent Parent window.
+   * @param account Managed account.
+   * @param serverName Managed server.
+   */
+  public AccountServerPanelController(WindowController parent, Account account, String serverName)
+  {
+    _parent=parent;
+    _account=account;
+    _server=serverName;
+    _windowsManager=new WindowsManager();
+  }
+
+  /**
+   * Get the managed panel.
+   * @return a panel.
+   */
+  public JPanel getPanel()
+  {
+    if (_panel==null)
+    {
+      _panel=buildPanel();
+    }
+    return _panel;
+  }
+
+  private JPanel buildPanel()
+  {
+    JPanel panel=GuiFactory.buildBackgroundPanel(new BorderLayout());
+    // Characters table
+    JPanel tablePanel=buildTablePanel();
+    tablePanel.setBorder(GuiFactory.buildTitledBorder("Characters"));
+    panel.add(tablePanel,BorderLayout.CENTER);
+    // Command buttons
+    JPanel commandsPanel=buildCommandsPanel();
+    panel.add(commandsPanel,BorderLayout.SOUTH);
+    return panel;
+  }
+
+  private JPanel buildTablePanel()
+  {
+    JPanel ret=GuiFactory.buildPanel(new BorderLayout());
+    _toonsTable=buildToonsTable();
+    JTable table=_toonsTable.getTable();
+    JScrollPane scroll=GuiFactory.buildScrollPane(table);
+    ret.add(scroll,BorderLayout.CENTER);
+    return ret;
+  }
+
+  private ToonsTableController buildToonsTable()
+  {
+    ToonsTableController tableController=new ToonsTableController();
+    List<CharacterFile> characters=AccountUtils.getCharacters(_account.getName(),_server);
+    tableController.setToons(characters);
+    return tableController;
+  }
+
+  private JPanel buildCommandsPanel()
+  {
+    JPanel panel=GuiFactory.buildPanel(new FlowLayout(FlowLayout.LEFT));
+    // Storage
+    JButton storageButton=buildCommandButton("Storage",STORAGE_COMMAND);
+    panel.add(storageButton);
+
+    return panel;
+  }
+
+  private JButton buildCommandButton(String label, String command)
+  {
+    JButton b=GuiFactory.buildButton(label);
+    b.setActionCommand(command);
+    b.addActionListener(this);
+    return b;
+  }
+
+  /**
+   * Handle button actions.
+   * @param e Source event.
+   */
+  @Override
+  public void actionPerformed(ActionEvent e)
+  {
+    String command=e.getActionCommand();
+    if (STORAGE_COMMAND.equals(command))
+    {
+      showStorage();
+    }
+  }
+
+  private void showStorage()
+  {
+    AccountStorageDisplayWindowController summaryController=(AccountStorageDisplayWindowController)_windowsManager.getWindow(AccountStorageDisplayWindowController.IDENTIFIER);
+    if (summaryController==null)
+    {
+      summaryController=new AccountStorageDisplayWindowController(_parent,_account,_server);
+      _windowsManager.registerWindow(summaryController);
+      summaryController.getWindow().setLocationRelativeTo(_parent.getWindow());
+    }
+    summaryController.bringToFront();
+  }
+
+  /**
+   * Release all managed resources.
+   */
+  public void dispose()
+  {
+    // Data
+    _account=null;
+    _server=null;
+    // UI
+    if (_panel!=null)
+    {
+      _panel.removeAll();
+      _panel=null;
+    }
+    // Controllers
+    if (_windowsManager!=null)
+    {
+      _windowsManager.disposeAll();
+      _windowsManager=null;
+    }
+    if (_toonsTable!=null)
+    {
+      _toonsTable.dispose();
+      _toonsTable=null;
+    }
+  }
+}
--- src/main/java/delta/games/lotro/gui/account/AccountWindowController.java
+++ src/main/java/delta/games/lotro/gui/account/AccountWindowController.java
@@ -2,42 +2,29 @@ package delta.games.lotro.gui.account;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTable;
+import javax.swing.JTabbedPane;
 
 import delta.common.ui.swing.GuiFactory;
 import delta.common.ui.swing.windows.DefaultWindowController;
-import delta.common.ui.swing.windows.WindowsManager;
 import delta.games.lotro.account.Account;
 import delta.games.lotro.account.AccountUtils;
-import delta.games.lotro.character.CharacterFile;
-import delta.games.lotro.gui.character.storage.AccountStorageDisplayWindowController;
-import delta.games.lotro.gui.toon.ToonsTableController;
 
 /**
  * Controller for an "account" window.
  * @author DAM
  */
-public class AccountWindowController extends DefaultWindowController implements ActionListener
+public class AccountWindowController extends DefaultWindowController
 {
-  private static final String STORAGE_COMMAND="storage";
-  private static final String SERVER="Landroval";
-
   private AccountSummaryPanelController _summaryController;
-  private ToonsTableController _toonsTable;
   private Account _account;
-  private WindowsManager _windowsManager;
+  private Map<String,AccountServerPanelController> _serverPanels;
 
   /**
    * Constructor.
@@ -46,7 +33,7 @@ public class AccountWindowController extends DefaultWindowController implements 
   public AccountWindowController(Account account)
   {
     _account=account;
-    _windowsManager=new WindowsManager();
+    _serverPanels=new HashMap<String,AccountServerPanelController>();
     _summaryController=new AccountSummaryPanelController(this,_account);
   }
 
@@ -65,20 +52,27 @@ public class AccountWindowController extends DefaultWindowController implements 
   protected JComponent buildContents()
   {
     // Whole panel
-    JPanel panel=GuiFactory.buildPanel(new GridBagLayout());
+    JPanel panel=GuiFactory.buildPanel(new BorderLayout());
     // Summary panel
     JPanel summaryPanel=_summaryController.getPanel();
-    GridBagConstraints c=new GridBagConstraints(0,0,2,1,1,0,GridBagConstraints.WEST,GridBagConstraints.HORIZONTAL,new Insets(3,5,3,5),0,0);
-    panel.add(summaryPanel,c);
-    // Characters table
-    JPanel tablePanel=buildTablePanel();
-    c=new GridBagConstraints(0,1,1,1,1.0,1.0,GridBagConstraints.NORTHWEST,GridBagConstraints.BOTH,new Insets(0,0,0,0),0,0);
-    panel.add(tablePanel,c);
-    tablePanel.setBorder(GuiFactory.buildTitledBorder("Characters"));
-    // Command buttons
-    JPanel commandsPanel=buildCommandsPanel();
-    c=new GridBagConstraints(0,2,1,1,0,0,GridBagConstraints.WEST,GridBagConstraints.NONE,new Insets(0,0,0,0),0,0);
-    panel.add(commandsPanel,c);
+    panel.add(summaryPanel,BorderLayout.NORTH);
+    // Servers tab
+    JTabbedPane tabbedPane=GuiFactory.buildTabbedPane();
+    List<String> servers=AccountUtils.getServers(_account.getName());
+    for(String server : servers)
+    {
+      JPanel serverPanel=buildServerPanel(server);
+      tabbedPane.add(server,serverPanel);
+    }
+    panel.add(tabbedPane,BorderLayout.CENTER);
+    return panel;
+  }
+
+  private JPanel buildServerPanel(String serverName)
+  {
+    AccountServerPanelController controller=new AccountServerPanelController(this,_account,serverName);
+    _serverPanels.put(serverName,controller);
+    JPanel panel=controller.getPanel();
     return panel;
   }
 
@@ -104,91 +98,25 @@ public class AccountWindowController extends DefaultWindowController implements 
     return id;
   }
 
-  private JPanel buildCommandsPanel()
-  {
-    JPanel panel=GuiFactory.buildPanel(new GridBagLayout());
-    GridBagConstraints c=new GridBagConstraints(0,0,1,1,0,0,GridBagConstraints.CENTER,GridBagConstraints.NONE,new Insets(5,5,5,5),0,0);
-    // Storage
-    JButton storageButton=buildCommandButton("Storage",STORAGE_COMMAND);
-    panel.add(storageButton,c);
-    c.gridx++;
-
-    return panel;
-  }
-
-  private JButton buildCommandButton(String label, String command)
-  {
-    JButton b=GuiFactory.buildButton(label);
-    b.setActionCommand(command);
-    b.addActionListener(this);
-    return b;
-  }
-
-  /**
-   * Handle button actions.
-   * @param e Source event.
-   */
-  @Override
-  public void actionPerformed(ActionEvent e)
-  {
-    String command=e.getActionCommand();
-    if (STORAGE_COMMAND.equals(command))
-    {
-      showStorage();
-    }
-  }
-
-  private JPanel buildTablePanel()
-  {
-    JPanel ret=GuiFactory.buildBackgroundPanel(new BorderLayout());
-    _toonsTable=buildToonsTable();
-    JTable table=_toonsTable.getTable();
-    JScrollPane scroll=GuiFactory.buildScrollPane(table);
-    ret.add(scroll,BorderLayout.CENTER);
-    return ret;
-  }
-
-  private ToonsTableController buildToonsTable()
-  {
-    ToonsTableController tableController=new ToonsTableController();
-    List<CharacterFile> characters=AccountUtils.getCharacters(_account.getName(),SERVER);
-    tableController.setToons(characters);
-    return tableController;
-  }
-
-  private void showStorage()
-  {
-    AccountStorageDisplayWindowController summaryController=(AccountStorageDisplayWindowController)_windowsManager.getWindow(AccountStorageDisplayWindowController.IDENTIFIER);
-    if (summaryController==null)
-    {
-      summaryController=new AccountStorageDisplayWindowController(this,_account,SERVER);
-      _windowsManager.registerWindow(summaryController);
-      summaryController.getWindow().setLocationRelativeTo(getWindow());
-    }
-    summaryController.bringToFront();
-  }
-
   /**
    * Release all managed resources.
    */
   @Override
   public void dispose()
   {
-    if (_windowsManager!=null)
-    {
-      _windowsManager.disposeAll();
-      _windowsManager=null;
-    }
     super.dispose();
     if (_summaryController!=null)
     {
       _summaryController.dispose();
       _summaryController=null;
     }
-    if (_toonsTable!=null)
+    if (_serverPanels!=null)
     {
-      _toonsTable.dispose();
-      _toonsTable=null;
+      for(AccountServerPanelController serverPanel : _serverPanels.values())
+      {
+        serverPanel.dispose();
+      }
+      _serverPanels=null;
     }
     if (_account!=null)
     {
