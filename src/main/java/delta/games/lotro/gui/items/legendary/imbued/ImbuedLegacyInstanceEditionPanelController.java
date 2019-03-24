08b6f7eaeb867c2a88211bde3efc5db2c38b5f08
Damien Morcellet <dmorcellet@free.fr>
2019-03-24 09:49:45 +0100

    Misc improvements in the imbued legacies edition

--- src/main/java/delta/games/lotro/gui/items/legendary/imbued/ImbuedLegacyInstanceEditionPanelController.java
+++ src/main/java/delta/games/lotro/gui/items/legendary/imbued/ImbuedLegacyInstanceEditionPanelController.java
@@ -1,13 +1,17 @@
 package delta.games.lotro.gui.items.legendary.imbued;
 
+import java.awt.Component;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.swing.Box;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingConstants;
 
 import delta.common.ui.swing.GuiFactory;
 import delta.common.ui.swing.combobox.ComboBoxController;
@@ -79,6 +83,31 @@ public class ImbuedLegacyInstanceEditionPanelController
   private JPanel build()
   {
     JPanel panel=GuiFactory.buildBackgroundPanel(new GridBagLayout());
+    int y=0;
+    // Headers
+    {
+      // - line 1
+      GridBagConstraints c=new GridBagConstraints(0,y,1,1,0.0,0.0,GridBagConstraints.WEST,GridBagConstraints.HORIZONTAL,new Insets(0,0,0,0),0,0);
+      Component valueStrut=Box.createHorizontalStrut(200);
+      panel.add(valueStrut,c);
+      c=new GridBagConstraints(1,y,2,1,0.0,0.0,GridBagConstraints.CENTER,GridBagConstraints.HORIZONTAL,new Insets(0,0,0,0),0,0);
+      panel.add(buildCenteredLabel("Levels"),c);
+      y++;
+      // - line 2
+      int x=0;
+      c=new GridBagConstraints(x,0,1,2,0.0,0.0,GridBagConstraints.CENTER,GridBagConstraints.HORIZONTAL,new Insets(0,0,0,0),0,0);
+      panel.add(buildCenteredLabel("Stats"),c);
+      panel.add(valueStrut,c);
+      x++;
+      c=new GridBagConstraints(x,y,1,1,0.0,0.0,GridBagConstraints.WEST,GridBagConstraints.HORIZONTAL,new Insets(0,0,0,5),0,0);
+      panel.add(buildCenteredLabel("Current"),c);
+      x++;
+      c=new GridBagConstraints(x,y,1,1,0.0,0.0,GridBagConstraints.WEST,GridBagConstraints.HORIZONTAL,new Insets(0,0,0,0),0,0);
+      panel.add(buildCenteredLabel("Max"),c);
+      x++;
+      y++;
+    }
+    // Rows
     int nbEditors=_editors.size();
     for(int i=0;i<nbEditors;i++)
     {
@@ -86,26 +115,34 @@ public class ImbuedLegacyInstanceEditionPanelController
       SingleImbuedLegacyEditionController editor=_editors.get(i);
       // Value
       MultilineLabel2 valueLabel=editor.getValueLabel();
-      GridBagConstraints c=new GridBagConstraints(x,i,1,1,1.0,0.0,GridBagConstraints.WEST,GridBagConstraints.HORIZONTAL,new Insets(5,5,5,5),0,0);
+      GridBagConstraints c=new GridBagConstraints(x,y,1,1,1.0,0.0,GridBagConstraints.WEST,GridBagConstraints.HORIZONTAL,new Insets(5,5,5,5),0,0);
       panel.add(valueLabel,c);
       x++;
       // Current level
       ComboBoxController<Integer> currentLevelCombo=editor.getCurrentLevelCombo();
-      c=new GridBagConstraints(x,i,1,1,0.0,0.0,GridBagConstraints.WEST,GridBagConstraints.NONE,new Insets(5,5,5,0),0,0);
+      c=new GridBagConstraints(x,y,1,1,0.0,0.0,GridBagConstraints.WEST,GridBagConstraints.NONE,new Insets(5,5,5,0),0,0);
       panel.add(currentLevelCombo.getComboBox(),c);
       x++;
       // Max level
       ComboBoxController<Integer> maxLevelCombo=editor.getMaxLevelCombo();
-      c=new GridBagConstraints(x,i,1,1,0.0,0.0,GridBagConstraints.WEST,GridBagConstraints.NONE,new Insets(5,5,5,5),0,0);
+      c=new GridBagConstraints(x,y,1,1,0.0,0.0,GridBagConstraints.WEST,GridBagConstraints.NONE,new Insets(5,5,5,5),0,0);
       panel.add(maxLevelCombo.getComboBox(),c);
       x++;
       // Button
       JButton chooseButton=editor.getChooseButton();
-      c=new GridBagConstraints(x,i,1,1,0.0,0.0,GridBagConstraints.WEST,GridBagConstraints.NONE,new Insets(5,5,5,5),0,0);
+      c=new GridBagConstraints(x,y,1,1,0.0,0.0,GridBagConstraints.WEST,GridBagConstraints.NONE,new Insets(5,5,5,5),0,0);
       panel.add(chooseButton,c);
       x++;
+      y++;
     }
     return panel;
+  }
+
+  private JLabel buildCenteredLabel(String text)
+  {
+    JLabel label=GuiFactory.buildLabel(text);
+    label.setHorizontalAlignment(SwingConstants.CENTER);
+    return label;
   }
 
   /**
