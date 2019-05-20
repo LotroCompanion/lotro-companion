e784f2c5d7b2b343f6fb885adbbcafb0e3067ba7
Damien Morcellet <dmorcellet@free.fr>
2019-05-20 22:26:15 +0200

    Added 'lore info' and 'progress override' display in quests/deeds forms

--- src/main/java/delta/games/lotro/gui/quests/ObjectivesHtmlBuilder.java
+++ src/main/java/delta/games/lotro/gui/quests/ObjectivesHtmlBuilder.java
@@ -6,6 +6,7 @@ import delta.games.lotro.gui.common.navigator.ReferenceConstants;
 import delta.games.lotro.lore.deeds.DeedDescription;
 import delta.games.lotro.lore.quests.Achievable;
 import delta.games.lotro.lore.quests.QuestDescription;
+import delta.games.lotro.lore.quests.objectives.DefaultObjectiveCondition;
 import delta.games.lotro.lore.quests.objectives.Objective;
 import delta.games.lotro.lore.quests.objectives.ObjectiveCondition;
 import delta.games.lotro.lore.quests.objectives.ObjectivesManager;
@@ -49,32 +50,56 @@ public class ObjectivesHtmlBuilder
     if (condition instanceof QuestCompleteCondition)
     {
       QuestCompleteCondition questComplete=(QuestCompleteCondition)condition;
-      //int count=questComplete.getCompletionCount();
-      //String questCategory=questComplete.getQuestCategory();
-      // Deed/quest
-      Proxy<Achievable> proxy=questComplete.getProxy();
-      if (proxy!=null)
+      handleQuestCompleteCondition(sb,questComplete);
+    }
+    else if (condition instanceof DefaultObjectiveCondition)
+    {
+      DefaultObjectiveCondition defaultCondition=(DefaultObjectiveCondition)condition;
+      handleDefaultCondition(sb,defaultCondition);
+    }
+  }
+
+  private static void handleQuestCompleteCondition(StringBuilder sb, QuestCompleteCondition questComplete)
+  {
+    //int count=questComplete.getCompletionCount();
+    //String questCategory=questComplete.getQuestCategory();
+    // Deed/quest
+    Proxy<Achievable> proxy=questComplete.getProxy();
+    if (proxy!=null)
+    {
+      Achievable achievable=proxy.getObject();
+      if (achievable instanceof QuestDescription)
       {
-        Achievable achievable=proxy.getObject();
-        if (achievable instanceof QuestDescription)
-        {
-          QuestDescription quest=(QuestDescription)achievable;
-          sb.append("<p>Complete quest <b>");
-          String text=quest.getName();
-          String to=ReferenceConstants.getAchievableReference(quest);
-          printLink(sb,to,text);
-          sb.append("</b></p>");
-        }
-        else if (achievable instanceof DeedDescription)
-        {
-          DeedDescription deed=(DeedDescription)achievable;
-          sb.append("<p>Complete deed <b>");
-          String text=deed.getName();
-          String to=ReferenceConstants.getAchievableReference(deed);
-          printLink(sb,to,text);
-          sb.append("</b></p>");
-        }
+        QuestDescription quest=(QuestDescription)achievable;
+        sb.append("<p>Complete quest <b>");
+        String text=quest.getName();
+        String to=ReferenceConstants.getAchievableReference(quest);
+        printLink(sb,to,text);
+        sb.append("</b></p>");
       }
+      else if (achievable instanceof DeedDescription)
+      {
+        DeedDescription deed=(DeedDescription)achievable;
+        sb.append("<p>Complete deed <b>");
+        String text=deed.getName();
+        String to=ReferenceConstants.getAchievableReference(deed);
+        printLink(sb,to,text);
+        sb.append("</b></p>");
+      }
+    }
+  }
+
+  private static void handleDefaultCondition(StringBuilder sb, DefaultObjectiveCondition condition)
+  {
+    String progressOverride=condition.getProgressOverride();
+    if ((progressOverride!=null) && (progressOverride.length()>0))
+    {
+      sb.append("<p>").append(toHtml(progressOverride)).append("</p>");
+    }
+    String loreInfo=condition.getLoreInfo();
+    if ((loreInfo!=null) && (loreInfo.length()>0))
+    {
+      sb.append("<p><i>").append(toHtml(loreInfo)).append("<i></p>");
     }
   }
 
