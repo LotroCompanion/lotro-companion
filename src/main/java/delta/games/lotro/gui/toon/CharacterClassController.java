769aa08a19b7e0e1122438437889bdf6de1d81df
Damien Morcellet <dmorcellet@free.fr>
2019-09-26 19:09:58 +0200

    Use allowed classes from race descriptions

--- src/main/java/delta/games/lotro/gui/toon/CharacterClassController.java
+++ src/main/java/delta/games/lotro/gui/toon/CharacterClassController.java
@@ -1,10 +1,11 @@
 package delta.games.lotro.gui.toon;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
 import delta.common.ui.swing.combobox.ComboBoxController;
+import delta.games.lotro.character.races.RaceDescription;
+import delta.games.lotro.character.races.RacesManager;
 import delta.games.lotro.common.CharacterClass;
 import delta.games.lotro.common.Race;
 
@@ -17,14 +18,11 @@ public class CharacterClassController
   private ComboBoxController<CharacterClass> _classController;
   private Race _race;
 
-  private HashMap<Race,List<CharacterClass>> _classesByRace;
-
   /**
    * Constructor.
    */
   public CharacterClassController()
   {
-    initClasses();
     _classController=new ComboBoxController<CharacterClass>();
     setRace(Race.ALL_RACES[0]);
   }
@@ -46,14 +44,14 @@ public class CharacterClassController
   {
     if (_race!=race)
     {
-      List<CharacterClass> classes=_classesByRace.get(race);
+      List<CharacterClass> classes=getClassesForRace(race);
       for(CharacterClass cClass : classes)
       {
         _classController.addItem(cClass,cClass.getLabel());
       }
       if (_race!=null)
       {
-        List<CharacterClass> oldClasses=_classesByRace.get(_race);
+        List<CharacterClass> oldClasses=getClassesForRace(_race);
         for(CharacterClass cClass : oldClasses)
         {
           _classController.removeItem(cClass);
@@ -63,66 +61,15 @@ public class CharacterClassController
     }
   }
 
-  private void initClasses()
+  private List<CharacterClass> getClassesForRace(Race race)
   {
-    _classesByRace=new HashMap<Race,List<CharacterClass>>();
-    // Men
-    List<CharacterClass> menClasses=new ArrayList<CharacterClass>();
-    menClasses.add(CharacterClass.BURGLAR);
-    menClasses.add(CharacterClass.CAPTAIN);
-    menClasses.add(CharacterClass.CHAMPION);
-    menClasses.add(CharacterClass.GUARDIAN);
-    menClasses.add(CharacterClass.HUNTER);
-    menClasses.add(CharacterClass.LORE_MASTER);
-    menClasses.add(CharacterClass.MINSTREL);
-    menClasses.add(CharacterClass.WARDEN);
-    _classesByRace.put(Race.MAN,menClasses);
-
-    // Elves
-    List<CharacterClass> elvesClasses=new ArrayList<CharacterClass>();
-    elvesClasses.add(CharacterClass.CHAMPION);
-    elvesClasses.add(CharacterClass.GUARDIAN);
-    elvesClasses.add(CharacterClass.HUNTER);
-    elvesClasses.add(CharacterClass.LORE_MASTER);
-    elvesClasses.add(CharacterClass.MINSTREL);
-    elvesClasses.add(CharacterClass.RUNE_KEEPER);
-    elvesClasses.add(CharacterClass.WARDEN);
-    _classesByRace.put(Race.ELF,elvesClasses);
-
-    // Dwarves
-    List<CharacterClass> dwarvesClasses=new ArrayList<CharacterClass>();
-    dwarvesClasses.add(CharacterClass.CHAMPION);
-    dwarvesClasses.add(CharacterClass.GUARDIAN);
-    dwarvesClasses.add(CharacterClass.HUNTER);
-    dwarvesClasses.add(CharacterClass.MINSTREL);
-    dwarvesClasses.add(CharacterClass.RUNE_KEEPER);
-    _classesByRace.put(Race.DWARF,dwarvesClasses);
-
-    // Hobbit
-    List<CharacterClass> hobbitsClasses=new ArrayList<CharacterClass>();
-    hobbitsClasses.add(CharacterClass.BURGLAR);
-    hobbitsClasses.add(CharacterClass.GUARDIAN);
-    hobbitsClasses.add(CharacterClass.HUNTER);
-    hobbitsClasses.add(CharacterClass.MINSTREL);
-    hobbitsClasses.add(CharacterClass.WARDEN);
-    _classesByRace.put(Race.HOBBIT,hobbitsClasses);
-
-    // Beorning
-    List<CharacterClass> beorningsClasses=new ArrayList<CharacterClass>();
-    beorningsClasses.add(CharacterClass.BEORNING);
-    _classesByRace.put(Race.BEORNING,beorningsClasses);
-
-    // High Elf
-    List<CharacterClass> highElvesClasses=new ArrayList<CharacterClass>();
-    highElvesClasses.add(CharacterClass.CAPTAIN);
-    highElvesClasses.add(CharacterClass.CHAMPION);
-    highElvesClasses.add(CharacterClass.GUARDIAN);
-    highElvesClasses.add(CharacterClass.HUNTER);
-    highElvesClasses.add(CharacterClass.LORE_MASTER);
-    highElvesClasses.add(CharacterClass.MINSTREL);
-    highElvesClasses.add(CharacterClass.RUNE_KEEPER);
-    highElvesClasses.add(CharacterClass.WARDEN);
-    _classesByRace.put(Race.HIGH_ELF,highElvesClasses);
+    List<CharacterClass> ret=new ArrayList<CharacterClass>();
+    RaceDescription description=RacesManager.getInstance().getRaceDescription(race);
+    if (description!=null)
+    {
+      ret.addAll(description.getAllowedClasses());
+    }
+    return ret;
   }
 
   /**
@@ -135,7 +82,6 @@ public class CharacterClassController
       _classController.dispose();
       _classController=null;
     }
-    _classesByRace=null;
     _race=null;
   }
 }
