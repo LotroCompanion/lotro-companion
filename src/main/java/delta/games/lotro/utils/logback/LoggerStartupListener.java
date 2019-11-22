049138e512811a39dec68514a88bb96c65a6ab85
Damien Morcellet <dmorcellet@free.fr>
2019-11-22 12:58:04 +0100

    Added a logback LoggerStartupListener to setup a property for the user home

---
+++ src/main/java/delta/games/lotro/utils/logback/LoggerStartupListener.java
@@ -0,0 +1,70 @@
+package delta.games.lotro.utils.logback;
+
+import ch.qos.logback.classic.Level;
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.spi.LoggerContextListener;
+import ch.qos.logback.core.Context;
+import ch.qos.logback.core.spi.ContextAwareBase;
+import ch.qos.logback.core.spi.LifeCycle;
+
+/**
+ * Logger startup listener to setup a property for the user home.
+ * @author DAM
+ */
+public class LoggerStartupListener extends ContextAwareBase implements LoggerContextListener, LifeCycle
+{
+  private boolean started=false;
+
+  @Override
+  public void start()
+  {
+    if (started) return;
+    String userHome=System.getProperty("user.home");
+    Context loggerContext=getContext();
+    loggerContext.putProperty("DATA_HOME",userHome);
+    started=true;
+  }
+
+  @Override
+  public void stop()
+  {
+    // Nothing
+  }
+
+  @Override
+  public boolean isStarted()
+  {
+    return started;
+  }
+
+  @Override
+  public boolean isResetResistant()
+  {
+    return true;
+  }
+
+  @Override
+  public void onStart(LoggerContext loggerContext)
+  {
+    // Nothing
+  }
+
+  @Override
+  public void onReset(LoggerContext loggerContext)
+  {
+    // Nothing
+  }
+
+  @Override
+  public void onStop(LoggerContext loggerContext)
+  {
+    // Nothing
+  }
+
+  @Override
+  public void onLevelChange(Logger logger, Level level)
+  {
+    // Nothing
+  }
+}
