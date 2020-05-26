a8e3b35d26d41b3a594f354eea2e190fac26883c
Damien Morcellet <dmorcellet@free.fr>
2020-05-26 23:15:22 +0200

    Added columns in the main characters table to show character details

--- src/main/java/delta/games/lotro/gui/toon/ToonsTableController.java
+++ src/main/java/delta/games/lotro/gui/toon/ToonsTableController.java
@@ -5,19 +5,30 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.table.DefaultTableCellRenderer;
 
 import delta.common.ui.swing.tables.CellDataProvider;
 import delta.common.ui.swing.tables.DefaultTableColumnController;
 import delta.common.ui.swing.tables.GenericTableController;
+import delta.common.ui.swing.tables.GenericTableController.DateRenderer;
 import delta.common.ui.swing.tables.ListDataProvider;
 import delta.common.ui.swing.tables.Sort;
 import delta.games.lotro.character.CharacterFile;
 import delta.games.lotro.character.CharacterSummary;
+import delta.games.lotro.character.details.CharacterDetails;
 import delta.games.lotro.character.events.CharacterEvent;
 import delta.games.lotro.character.events.CharacterEventType;
 import delta.games.lotro.common.CharacterClass;
 import delta.games.lotro.common.CharacterSex;
+import delta.games.lotro.common.Duration;
 import delta.games.lotro.common.Race;
+import delta.games.lotro.common.money.Money;
+import delta.games.lotro.common.money.comparator.MoneyComparator;
+import delta.games.lotro.gui.items.chooser.ItemsTableBuilder;
+import delta.games.lotro.lore.titles.TitleDescription;
+import delta.games.lotro.lore.titles.TitlesManager;
+import delta.games.lotro.utils.Formats;
 import delta.games.lotro.utils.events.EventsManager;
 import delta.games.lotro.utils.events.GenericEventsListener;
 
@@ -186,9 +197,117 @@ public class ToonsTableController implements GenericEventsListener<CharacterEven
       table.addColumnController(lastUpdateColumn);
     }
     */
+    List<DefaultTableColumnController<CharacterFile,?>> detailsColumns=getDetailsColumns();
+    for(DefaultTableColumnController<CharacterFile,?> column : detailsColumns)
+    {
+      table.addColumnController(column);
+    }
     String sort=Sort.SORT_ASCENDING+NAME+Sort.SORT_ITEM_SEPARATOR+Sort.SORT_ASCENDING+SERVER;
     table.setSort(Sort.buildFromString(sort));
     return table;
+  }
+
+  private List<DefaultTableColumnController<CharacterFile,?>> getDetailsColumns()
+  {
+    List<DefaultTableColumnController<CharacterFile,?>> ret=new ArrayList<DefaultTableColumnController<CharacterFile,?>>();
+    // XP column
+    {
+      CellDataProvider<CharacterFile,Long> xpCell=new CellDataProvider<CharacterFile,Long>()
+      {
+        @Override
+        public Long getData(CharacterFile file)
+        {
+          CharacterDetails data=file.getDetails();
+          return Long.valueOf(data.getXp());
+        }
+      };
+      DefaultTableColumnController<CharacterFile,Long> xpColumn=new DefaultTableColumnController<CharacterFile,Long>("XP","XP",Long.class,xpCell);
+      xpColumn.setWidthSpecs(80,80,80);
+      ret.add(xpColumn);
+    }
+    // In-game time column
+    {
+      CellDataProvider<CharacterFile,Integer> cooldownCell=new CellDataProvider<CharacterFile,Integer>()
+      {
+        @Override
+        public Integer getData(CharacterFile file)
+        {
+          CharacterDetails data=file.getDetails();
+          return Integer.valueOf(data.getIngameTime());
+        }
+      };
+      DefaultTableColumnController<CharacterFile,Integer> cooldownColumn=new DefaultTableColumnController<CharacterFile,Integer>("INGAME_TIME","In-game Time",Integer.class,cooldownCell);
+      cooldownColumn.setWidthSpecs(120,120,120);
+      DefaultTableCellRenderer renderer=new DefaultTableCellRenderer()
+      {
+        @Override
+        public void setValue(Object value)
+        {
+          setHorizontalAlignment(SwingConstants.CENTER);
+          setText((value == null) ? "" : Duration.getDurationString(((Integer)value).intValue()));
+        }
+      };
+      cooldownColumn.setCellRenderer(renderer);
+      ret.add(cooldownColumn);
+    }
+    // Money
+    {
+      CellDataProvider<CharacterFile,Money> moneyCell=new CellDataProvider<CharacterFile,Money>()
+      {
+        @Override
+        public Money getData(CharacterFile file)
+        {
+          CharacterDetails data=file.getDetails();
+          return data.getMoney();
+        }
+      };
+      DefaultTableColumnController<CharacterFile,Money> moneyColumn=new DefaultTableColumnController<CharacterFile,Money>("MONEY","Money",Money.class,moneyCell);
+      moneyColumn.setWidthSpecs(120,180,180);
+      moneyColumn.setCellRenderer(ItemsTableBuilder.buildMoneyCellRenderer());
+      moneyColumn.setComparator(new MoneyComparator());
+      ret.add(moneyColumn);
+    }
+    // Last logout time
+    {
+      CellDataProvider<CharacterFile,Long> lastLogoutCell=new CellDataProvider<CharacterFile,Long>()
+      {
+        public Long getData(CharacterFile file)
+        {
+          CharacterDetails data=file.getDetails();
+          return data.getLastLogoutDate();
+        }
+      };
+      DefaultTableColumnController<CharacterFile,Long> lastLogoutColumn=new DefaultTableColumnController<CharacterFile,Long>("LAST_LOGOUT_DATE","Last logout",Long.class,lastLogoutCell);
+      lastLogoutColumn.setWidthSpecs(120,120,120);
+      lastLogoutColumn.setCellRenderer(new DateRenderer(Formats.DATE_TIME_PATTERN));
+      ret.add(lastLogoutColumn);
+    }
+    // Title column
+    {
+      CellDataProvider<CharacterFile,String> titleCell=new CellDataProvider<CharacterFile,String>()
+      {
+        @Override
+        public String getData(CharacterFile file)
+        {
+          String titleName=null;
+          Integer titleId=file.getDetails().getCurrentTitleId();
+          if (titleId!=null)
+          {
+            TitleDescription title=TitlesManager.getInstance().getTitle(titleId.intValue());
+            if (title!=null)
+            {
+              titleName=title.getName();
+            }
+          }
+          return titleName;
+        }
+      };
+      DefaultTableColumnController<CharacterFile,String> titleColumn=new DefaultTableColumnController<CharacterFile,String>("TITLE","Title",String.class,titleCell);
+      titleColumn.setWidthSpecs(100,-1,200);
+      ret.add(titleColumn);
+    }
+
+    return ret;
   }
 
   /**
@@ -218,7 +337,8 @@ public class ToonsTableController implements GenericEventsListener<CharacterEven
   public void eventOccurred(CharacterEvent event)
   {
     CharacterEventType type=event.getType();
-    if (type==CharacterEventType.CHARACTER_SUMMARY_UPDATED)
+    if ((type==CharacterEventType.CHARACTER_SUMMARY_UPDATED)
+        || (type==CharacterEventType.CHARACTER_DETAILS_UPDATED))
     {
       CharacterFile toon=event.getToonFile();
       _tableController.refresh(toon);
