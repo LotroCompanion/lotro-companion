d656306e7eed33b929564568bc61ce2a4423559d
Damien Morcellet <dmorcellet@free.fr>
2021-06-11 23:13:48 +0200

    Added display of 4 new attributes of kinship summaries

--- src/main/java/delta/games/lotro/gui/kinship/KinshipSummaryDisplayPanelController.java
+++ src/main/java/delta/games/lotro/gui/kinship/KinshipSummaryDisplayPanelController.java
@@ -3,15 +3,20 @@ package delta.games.lotro.gui.kinship;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.util.Date;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import delta.common.ui.swing.GuiFactory;
+import delta.games.lotro.common.id.InternalGameId;
 import delta.games.lotro.kinship.Kinship;
+import delta.games.lotro.kinship.KinshipMember;
+import delta.games.lotro.kinship.KinshipRoster;
 import delta.games.lotro.kinship.KinshipSummary;
 import delta.games.lotro.kinship.events.KinshipEvent;
 import delta.games.lotro.kinship.events.KinshipEventType;
+import delta.games.lotro.utils.Formats;
 import delta.games.lotro.utils.events.EventsManager;
 import delta.games.lotro.utils.events.GenericEventsListener;
 
@@ -27,6 +32,10 @@ public class KinshipSummaryDisplayPanelController implements GenericEventsListen
   // UI
   private JPanel _panel;
   private JLabel _nameLabel;
+  private JLabel _founderLabel;
+  private JLabel _creationDateLabel;
+  private JLabel _leaderLabel;
+  private JLabel _motdLabel;
 
   /**
    * Constructor.
@@ -55,12 +64,49 @@ public class KinshipSummaryDisplayPanelController implements GenericEventsListen
   private JPanel buildPanel()
   {
     JPanel panel=GuiFactory.buildPanel(new GridBagLayout());
-    GridBagConstraints c=new GridBagConstraints(0,0,1,1,1.0,0,GridBagConstraints.WEST,GridBagConstraints.HORIZONTAL,new Insets(2,2,2,2),0,0);
 
     // Name
     _nameLabel=GuiFactory.buildLabel("",28.0f);
+    GridBagConstraints c=new GridBagConstraints(0,0,1,1,1.0,0,GridBagConstraints.WEST,GridBagConstraints.HORIZONTAL,new Insets(2,5,2,2),0,0);
     panel.add(_nameLabel,c);
+    // Attributes
+    JPanel attributesPanel=buildAttributesPanel();
+    c=new GridBagConstraints(0,1,1,1,1.0,0,GridBagConstraints.WEST,GridBagConstraints.HORIZONTAL,new Insets(2,2,2,2),0,0);
+    panel.add(attributesPanel,c);
+
     update();
+    return panel;
+  }
+
+  private JPanel buildAttributesPanel()
+  {
+    JPanel panel=GuiFactory.buildPanel(new GridBagLayout());
+    Insets insets=new Insets(2,5,2,5);
+    GridBagConstraints gbc=new GridBagConstraints(0,0,1,1,0.0,0.0,GridBagConstraints.WEST,GridBagConstraints.NONE,insets,0,0);
+    // Labels
+    panel.add(GuiFactory.buildLabel("Founder:"),gbc);
+    gbc.gridx=0; gbc.gridy++;
+    panel.add(GuiFactory.buildLabel("Creation Date:"),gbc);
+    gbc.gridx=0; gbc.gridy++;
+    panel.add(GuiFactory.buildLabel("Leader:"),gbc);
+    gbc.gridx=0; gbc.gridy++;
+    panel.add(GuiFactory.buildLabel("MOTD:"),gbc);
+    gbc.gridx=0; gbc.gridy++;
+    // Values
+    gbc.gridx=1; gbc.gridy=0;
+    gbc.weightx=1.0; gbc.fill=GridBagConstraints.HORIZONTAL;
+    _founderLabel=GuiFactory.buildLabel("");
+    panel.add(_founderLabel,gbc);
+    gbc.gridx=1; gbc.gridy++;
+    _creationDateLabel=GuiFactory.buildLabel("");
+    panel.add(_creationDateLabel,gbc);
+    gbc.gridx=1; gbc.gridy++;
+    _leaderLabel=GuiFactory.buildLabel("");
+    panel.add(_leaderLabel,gbc);
+    gbc.gridx=1; gbc.gridy++;
+    _motdLabel=GuiFactory.buildLabel("");
+    panel.add(_motdLabel,gbc);
+    gbc.gridx=1; gbc.gridy++;
     return panel;
   }
 
@@ -87,12 +133,46 @@ public class KinshipSummaryDisplayPanelController implements GenericEventsListen
    */
   public void update()
   {
-    if (_summary!=null)
+    if (_summary==null)
     {
-      // Name
-      String name=_summary.getName();
-      _nameLabel.setText(name);
+      return;
     }
+    // Name
+    String name=_summary.getName();
+    _nameLabel.setText(name);
+    // Founder
+    String founder=resolveMemberName(_summary.getFounderID());
+    _founderLabel.setText(founder);
+    // Creation date
+    String dateStr="";
+    Long creationDate=_summary.getCreationDate();
+    if (creationDate!=null)
+    {
+      Date d=new Date(creationDate.longValue());
+      dateStr=Formats.getDateFormatter().format(d);
+    }
+    _creationDateLabel.setText(dateStr);
+    // Leader
+    String leader=resolveMemberName(_summary.getLeaderID());
+    _leaderLabel.setText(leader);
+    // Message of the day
+    String motd=_summary.getMotd();
+    _motdLabel.setText(motd);
+  }
+
+  private String resolveMemberName(InternalGameId memberId)
+  {
+    if (memberId==null)
+    {
+      return "-";
+    }
+    KinshipRoster roster=_kinship.getRoster();
+    KinshipMember member=roster.getMemberByID(memberId.asLong());
+    if (member!=null)
+    {
+      return member.getSummary().getName();
+    }
+    return "???";
   }
 
   /**
