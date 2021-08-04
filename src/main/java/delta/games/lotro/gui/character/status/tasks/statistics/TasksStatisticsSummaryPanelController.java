653e829926056eeeaa9de6b7af57962ad288aec4
Damien Morcellet <dmorcellet@free.fr>
2021-08-04 15:26:39 +0200

    Added XP for tasks statistics

--- src/main/java/delta/games/lotro/gui/character/status/tasks/statistics/TasksStatisticsSummaryPanelController.java
+++ src/main/java/delta/games/lotro/gui/character/status/tasks/statistics/TasksStatisticsSummaryPanelController.java
@@ -30,8 +30,11 @@ public class TasksStatisticsSummaryPanelController
   private JLabel _completionsCount;
   private JLabel _reputation;
   private JLabel _consumedItemsCount;
-  private JLabel _earnedItemsCount;
   private MoneyDisplayController _priceDisplay;
+  private JLabel _earnedItemsCount;
+  private JLabel _totalXP;
+  private JLabel _totalItemXP;
+  private JLabel _totalMountXP;
 
   /**
    * Constructor.
@@ -85,6 +88,21 @@ public class TasksStatisticsSummaryPanelController
     _earnedItemsCount=GuiFactory.buildLabel("");
     _statsPanel.add(_earnedItemsCount,cValues);
     cLabels.gridy++;cValues.gridy++;
+    // XP
+    _statsPanel.add(GuiFactory.buildLabel("Total XP:"),cLabels);
+    _totalXP=GuiFactory.buildLabel("");
+    _statsPanel.add(_totalXP,cValues);
+    cLabels.gridy++;cValues.gridy++;
+    // Item XP
+    _statsPanel.add(GuiFactory.buildLabel("Total Item XP:"),cLabels);
+    _totalItemXP=GuiFactory.buildLabel("");
+    _statsPanel.add(_totalItemXP,cValues);
+    cLabels.gridy++;cValues.gridy++;
+    // Mount XP
+    _statsPanel.add(GuiFactory.buildLabel("Total Mount XP:"),cLabels);
+    _totalMountXP=GuiFactory.buildLabel("");
+    _statsPanel.add(_totalMountXP,cValues);
+    cLabels.gridy++;cValues.gridy++;
 
     return panel;
   }
@@ -114,6 +132,10 @@ public class TasksStatisticsSummaryPanelController
     // Earned items
     ItemsStats earnedItemsStats=_statistics.getEarnedItemsStats();
     _earnedItemsCount.setText(getItemsLabel(earnedItemsStats));
+    // XP
+    _totalXP.setText(String.valueOf(_statistics.getTotalXP()));
+    _totalItemXP.setText(String.valueOf(_statistics.getTotalItemXP()));
+    _totalMountXP.setText(String.valueOf(_statistics.getTotalMountXP()));
   }
 
   private String getItemsLabel(ItemsStats stats)
@@ -156,5 +178,8 @@ public class TasksStatisticsSummaryPanelController
       _priceDisplay=null;
     }
     _earnedItemsCount=null;
+    _totalXP=null;
+    _totalItemXP=null;
+    _totalMountXP=null;
   }
 }
