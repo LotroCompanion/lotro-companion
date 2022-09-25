562205f84ea8c570e4978525b739fb4f20999bd6
Damien Morcellet <dmorcellet@free.fr>
2022-09-25 22:44:36 +0200

    Use item scaling facilities ; added a column for item value

--- src/main/java/delta/games/lotro/gui/lore/items/form/ItemScalableStatsPanelController.java
+++ src/main/java/delta/games/lotro/gui/lore/items/form/ItemScalableStatsPanelController.java
@@ -4,24 +4,20 @@ import java.awt.BorderLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import javax.swing.JPanel;
 
 import delta.common.ui.swing.GuiFactory;
 import delta.common.ui.swing.tables.RawTablePanelController;
-import delta.games.lotro.Config;
 import delta.games.lotro.character.stats.BasicStatsSet;
+import delta.games.lotro.common.money.Money;
 import delta.games.lotro.common.stats.StatDescription;
-import delta.games.lotro.common.stats.StatProvider;
 import delta.games.lotro.common.stats.StatUtils;
-import delta.games.lotro.common.stats.StatsProvider;
 import delta.games.lotro.lore.items.Item;
-import delta.games.lotro.lore.items.scaling.Munging;
-import delta.games.lotro.utils.maths.Progression;
+import delta.games.lotro.lore.items.scaling.ItemScaling;
+import delta.games.lotro.lore.items.scaling.ItemScalingBuilder;
+import delta.games.lotro.lore.items.scaling.ItemScalingEntry;
 
 /**
  * Controller for a panel to display a table of the scalable stats of an item.
@@ -55,30 +51,13 @@ public class ItemScalableStatsPanelController
 
   private JPanel build()
   {
+    ItemScaling scaling=ItemScalingBuilder.build(_item);
+    if (scaling.getEntries().size()==0)
+    {
+      return null;
+    }
     JPanel panel=null;
-    StatsProvider statsProvider=_item.getStatsProvider();
-    if (statsProvider==null)
-    {
-      return null;
-    }
-    Munging munging=_item.getMunging();
-    if (munging==null)
-    {
-      return null;
-    }
-    Progression progression=munging.getProgression();
-    if (progression==null)
-    {
-      return null;
-    }
-    Integer min=munging.getMin();
-    int minLevel=(min!=null?min.intValue():1);
-    Integer max=munging.getMax();
-    int levelCap=Config.getInstance().getMaxCharacterLevel();
-    int maxLevel=(max!=null?max.intValue():levelCap);
-
-    List<StatDescription> stats=getStats(statsProvider);
-    List<Integer> itemLevels=getItemLevels(minLevel,maxLevel,progression);
+    List<StatDescription> stats=scaling.getStats();
 
     // Headers
     List<String> headers=new ArrayList<String>();
@@ -88,14 +67,24 @@ public class ItemScalableStatsPanelController
     {
       headers.add(stat.getName());
     }
+    headers.add("Value");
     // Rows
     List<Object[]> rows=new ArrayList<Object[]>();
+    List<Integer> itemLevels=scaling.getItemLevels();
     for(Integer itemLevel : itemLevels)
     {
+      List<ItemScalingEntry> entries=scaling.getEntriesForItemLevel(itemLevel.intValue());
+      if (entries.size()==0)
+      {
+        continue;
+      }
       Object[] row=new Object[headers.size()];
-      row[0]=getLevelsForItemLevel(minLevel,maxLevel,progression,itemLevel.intValue());
+      int min=entries.get(0).getLevel();
+      int max=entries.get(entries.size()-1).getLevel();
+      row[0]=(min!=max)?String.valueOf(min)+"-"+max:String.valueOf(min);
       row[1]=itemLevel;
-      BasicStatsSet values=statsProvider.getStats(1,itemLevel.intValue());
+      ItemScalingEntry entry=entries.get(0);
+      BasicStatsSet values=entry.getStats();
       int index=2;
       for(StatDescription stat : stats)
       {
@@ -103,6 +92,8 @@ public class ItemScalableStatsPanelController
         row[index]=StatUtils.getStatDisplay(value,stat.isPercentage());
         index++;
       }
+      Money money=entry.getMoney();
+      row[index]=(money!=null)?money.getShortLabel():"-";
       rows.add(row);
     }
     RawTablePanelController tableController=new RawTablePanelController(headers,rows);
@@ -116,58 +107,6 @@ public class ItemScalableStatsPanelController
     JPanel paddingPanel=GuiFactory.buildPanel(new BorderLayout());
     GridBagConstraints c=new GridBagConstraints(nbCellsX,nbCellsY,1,1,1.0,1.0,GridBagConstraints.NORTHWEST,GridBagConstraints.BOTH,new Insets(0,0,0,0),0,0);
     panel.add(paddingPanel,c);
-  }
-
-  private List<StatDescription> getStats(StatsProvider statsProvider)
-  {
-    List<StatDescription> stats=new ArrayList<StatDescription>();
-    int nbStats=statsProvider.getNumberOfStatProviders();
-    for(int i=0;i<nbStats;i++)
-    {
-      StatProvider statProvider=statsProvider.getStatProvider(i);
-      StatDescription stat=statProvider.getStat();
-      stats.add(stat);
-    }
-    return stats;
-  }
-
-  private String getLevelsForItemLevel(int minLevel, int maxLevel, Progression progression, int itemLevel)
-  {
-    List<Integer> levels=new ArrayList<Integer>();
-    for(int level=minLevel;level<=maxLevel;level++)
-    {
-      Float itemLevelForLevel=progression.getValue(level);
-      if ((itemLevelForLevel!=null) && (itemLevelForLevel.intValue()==itemLevel))
-      {
-        levels.add(Integer.valueOf(level));
-      }
-    }
-    int nbLevels=levels.size();
-    if (nbLevels==1)
-    {
-      return levels.get(0).toString();
-    }
-    else if (nbLevels>1)
-    {
-      return levels.get(0)+"-"+levels.get(nbLevels-1);
-    }
-    return "";
-  }
-
-  private List<Integer> getItemLevels(int minLevel, int maxLevel, Progression progression)
-  {
-    Set<Integer> itemLevels=new HashSet<Integer>();
-    for(int level=minLevel;level<=maxLevel;level++)
-    {
-      Float itemLevel=progression.getValue(level);
-      if (itemLevel!=null)
-      {
-        itemLevels.add(Integer.valueOf(itemLevel.intValue()));
-      }
-    }
-    List<Integer> ret=new ArrayList<Integer>(itemLevels);
-    Collections.sort(ret);
-    return ret;
   }
 
   /**
