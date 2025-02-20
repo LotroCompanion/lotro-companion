9c831228525b590689dfe6f6f19d31498dd724f4
Damien Morcellet <dmorcellet@free.fr>
2025-02-20 16:42:09 +0100

    Add the kinship house summary in the character housing status panel

--- src/main/java/delta/games/lotro/gui/character/status/housing/CharacterHousingStatusPanelController.java
+++ src/main/java/delta/games/lotro/gui/character/status/housing/CharacterHousingStatusPanelController.java
@@ -11,9 +11,16 @@ import javax.swing.JPanel;
 import delta.common.ui.swing.GuiFactory;
 import delta.common.ui.swing.panels.AbstractPanelController;
 import delta.common.ui.swing.windows.WindowController;
+import delta.games.lotro.character.CharacterFile;
+import delta.games.lotro.character.CharacterSummary;
 import delta.games.lotro.character.status.housing.AccountHousingData;
+import delta.games.lotro.character.status.housing.HouseAddress;
 import delta.games.lotro.character.status.housing.HouseIdentifier;
 import delta.games.lotro.character.status.housing.HouseReference;
+import delta.games.lotro.common.id.InternalGameId;
+import delta.games.lotro.kinship.Kinship;
+import delta.games.lotro.kinship.KinshipSummary;
+import delta.games.lotro.kinship.KinshipsManager;
 
 /**
  * Panel to display a house status summary.
@@ -23,18 +30,21 @@ public class CharacterHousingStatusPanelController extends AbstractPanelControll
 {
   // Data
   private AccountHousingData _data;
+  private CharacterFile _character;
   // Controllers
   private List<HouseStatusSummaryPanelController> _houseStatusCtrls;
 
   /**
    * Constructor.
    * @param parent Parent window.
+   * @param character Current character.
    * @param data Data to show.
    */
-  public CharacterHousingStatusPanelController(WindowController parent, AccountHousingData data)
+  public CharacterHousingStatusPanelController(WindowController parent, CharacterFile character, AccountHousingData data)
   {
     super(parent);
     _data=data;
+    _character=character;
     _houseStatusCtrls=new ArrayList<HouseStatusSummaryPanelController>();
     setPanel(buildPanel());
   }
@@ -49,9 +59,20 @@ public class CharacterHousingStatusPanelController extends AbstractPanelControll
     {
       List<HouseReference> houses=new ArrayList<HouseReference>();
       houses.add(classicHouse);
-      JPanel classicHousesPanel=buildStatusPanel(houses);
-      classicHousesPanel.setBorder(GuiFactory.buildTitledBorder("Classic house"));
-      ret.add(classicHousesPanel,c);
+      JPanel classicHousePanel=buildStatusPanel(houses);
+      classicHousePanel.setBorder(GuiFactory.buildTitledBorder("Classic house"));
+      ret.add(classicHousePanel,c);
+      c.gridy++;
+    }
+    // Kinship house?
+    HouseReference kinshipHouse=getKinshipHouse();
+    if (kinshipHouse!=null)
+    {
+      List<HouseReference> houses=new ArrayList<HouseReference>();
+      houses.add(kinshipHouse);
+      JPanel kinshipHousePanel=buildStatusPanel(houses);
+      kinshipHousePanel.setBorder(GuiFactory.buildTitledBorder("Kinship house"));
+      ret.add(kinshipHousePanel,c);
       c.gridy++;
     }
     // Premium houses
@@ -63,6 +84,33 @@ public class CharacterHousingStatusPanelController extends AbstractPanelControll
       ret.add(premiumHousesPanel,c);
     }
     return ret;
+  }
+
+  private HouseReference getKinshipHouse()
+  {
+    CharacterSummary summary=_character.getSummary();
+    if (summary==null)
+    {
+      return null;
+    }
+    InternalGameId kinshipID=summary.getKinshipID();
+    if (kinshipID==null)
+    {
+      return null;
+    }
+    Kinship kinship=KinshipsManager.getInstance().getKinshipByID(kinshipID.asLong());
+    if (kinship==null)
+    {
+      return null;
+    }
+    KinshipSummary kinshipSummary=kinship.getSummary();
+    HouseAddress address=kinshipSummary.getAddress();
+    if (address==null)
+    {
+      return null;
+    }
+    HouseReference ref=new HouseReference(address,null);
+    return ref;
   }
 
   private JPanel buildStatusPanel(List<HouseReference> houses)
--- src/test/java/delta/games/lotro/gui/character/status/housing/MainTestCharacterHousingStatusPanelController.java
+++ src/test/java/delta/games/lotro/gui/character/status/housing/MainTestCharacterHousingStatusPanelController.java
@@ -1,9 +1,11 @@
 package delta.games.lotro.gui.character.status.housing;
 
 import delta.common.ui.swing.windows.DefaultWindowController;
+import delta.games.lotro.LotroTestUtils;
 import delta.games.lotro.account.Account;
 import delta.games.lotro.account.AccountOnServer;
 import delta.games.lotro.account.AccountsManager;
+import delta.games.lotro.character.CharacterFile;
 import delta.games.lotro.character.status.housing.AccountHousingData;
 import delta.games.lotro.character.status.housing.io.HousingStatusIO;
 
@@ -18,7 +20,8 @@ class MainTestCharacterHousingStatusPanelController
     Account account=AccountsManager.getInstance().getAccountByAccountName("glorfindel666");
     AccountOnServer accountOnServer=account.getServer("Landroval");
     AccountHousingData data=HousingStatusIO.loadAccountHousingData(accountOnServer);
-    CharacterHousingStatusPanelController ctrl=new CharacterHousingStatusPanelController(null,data);
+    CharacterFile character=new LotroTestUtils().getMainToon();
+    CharacterHousingStatusPanelController ctrl=new CharacterHousingStatusPanelController(null,character,data);
     DefaultWindowController w=new DefaultWindowController();
     w.getFrame().add(ctrl.getPanel());
     w.getFrame().pack();
