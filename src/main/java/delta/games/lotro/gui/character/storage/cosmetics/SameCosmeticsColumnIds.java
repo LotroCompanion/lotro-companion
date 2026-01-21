19fb840917fbd011a19a249dcfc7bcae2e9d16ca
Damien Morcellet <dmorcellet@free.fr>
2026-01-21 20:42:24 +0100

    Added an alternative panel for the 'same cosmetics' window, that uses a table

---
+++ src/main/java/delta/games/lotro/gui/character/storage/cosmetics/SameCosmeticsColumnIds.java
@@ -0,0 +1,17 @@
+package delta.games.lotro.gui.character.storage.cosmetics;
+
+/**
+ * Some column identifiers for the same cosmetics tables.
+ * @author DAM
+ */
+public enum SameCosmeticsColumnIds
+{
+  /**
+   * Group ID.
+   */
+  GROUP_ID,
+  /**
+   * Cosmetic ID.
+   */
+  COSMETIC_ID
+}
